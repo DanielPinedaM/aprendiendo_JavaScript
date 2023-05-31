3adf2173d70d9f0b3d01c85caf44c7a37990ab4a
daniel <daniel.pinedamo@amigo.edu.co>
2023-05-30 23:58:42 -0500

    fixup! 10_Recorrer_(iterar)/4_Array_de_objetos.js

--- src/10_Recorrer_(iterar)/4_Array_de_Objetos.js
+++ src/10_Recorrer_(iterar)/4_Array_de_Objetos.js
@@ -7,25 +7,25 @@ JUNTO las formas que hay para iterar arrays [] y objetos {}
 A continuacion solo resumi las principales formas,
 pero en este link hay mas formas:
 
-Techie Delight - Recorrer (Iterar) Array de Objetos [{}]
+Techie Delight - Recorrer (Iterar) Array de Objetos [ {} ]
 https://www.techiedelight.com/loop-through-array-of-objects-javascript/
 */
 
 const arrayDeObjetos = [
   {
-    uno: 1,
-    dos: 2,
+    uno: 'tres',
+    dos: 'cuatro',
   },
   {
-    tres: 3,
-    cuatro: 4,
+    uno: 'cinco',
+    dos: 'seis',
   },
 ];
 console.log(arrayDeObjetos);
 /*
 [
-  { uno: 1, dos: 2 },
-  { tres: 3, cuatro: 4 }
+  { uno: 'tres', dos: 'cuatro' },
+  { uno: 'cinco', dos: 'seis' }
 ]
 */
 
@@ -33,7 +33,10 @@ console.log(arrayDeObjetos);
  ▄▄▄▄▄▄▄▄▄▄
  █ .map() █
  ▀▀▀▀▀▀▀▀▀▀
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+.map() obtener los objetos {}
+q estan dentro del array de objetos [ {} ] */
 
 const getObjects = arrayDeObjetos.map((objetoLiteral) => {
   console.log(objetoLiteral);
@@ -41,17 +44,52 @@ const getObjects = arrayDeObjetos.map((objetoLiteral) => {
   return objetoLiteral;
 });
 /*
-{ uno: 1, dos: 2 }
-{ tres: 3, cuatro: 4 }
+{ uno: 'tres', dos: 'cuatro' }
+{ uno: 'cinco', dos: 'seis' }
 */
 
 console.log(getObjects);
 /*
 [
-  { uno: 1, dos: 2 },
-  { tres: 3, cuatro: 4 }
+  { uno: 'tres', dos: 'cuatro' },
+  { uno: 'cinco', dos: 'seis' }
 ]
 */
+
+/* .map() Iterar array de objetos [ {} ]
+SI conozco los nombres de las propiedades  */
+
+const getValue = arrayDeObjetos.map((objetoLiteral) => {
+  console.log(objetoLiteral.uno);
+  console.log(objetoLiteral.dos);
+
+  return [objetoLiteral.uno, objetoLiteral.dos];
+});
+/*
+'tres'
+'cuatro'
+'cinco'
+'seis'
+*/
+
+console.log(getValue.flat(Infinity));
+// [ 'tres', 'cuatro', 'cinco', 'seis' ]
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Object.keys() █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+
+Object.keys() Iterar array de objetos [ {} ]
+para obtener SOLAMENTE las propiedades
+cuando el nombre de las propiedades es DESCONOCIDA
+
+Se ELIMINAN los nombres de las propiedades repetidas */
+
+const getProperty = Object.keys(arrayDeObjetos[0]);
+console.log(getProperty);
+// (2) ['uno', 'dos']
 
 /*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
@@ -63,7 +101,18 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach */
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+Iterar array de objetos [ {} ]
+para obtener las propiedad: valor,
+cuando el nombre de las propiedades es DESCONOCIDO
+
+Se itera 2 veces:
+1) .map() obtener los objetos {}
+q estan dentro del array de objetos [ {} ]
+
+2) Object.entries() y .forEach()
+Obtener las propiedad: valor, */
 
 const getPropertyValue = arrayDeObjetos.map((objeto) => {
   Object.entries(objeto).forEach((entry) => {
@@ -74,16 +123,16 @@ const getPropertyValue = arrayDeObjetos.map((objeto) => {
   return objeto;
 });
 /*
-'uno    ➜ 1'
-'dos    ➜ 2'
-'tres   ➜ 3'
-'cuatro ➜ 4'
+'uno ➜ tres'
+'dos ➜ cuatro'
+'uno ➜ cinco'
+'dos ➜ seis'
 */
 
 console.log(getPropertyValue);
 /*
 [
-  { uno: 1, dos: 2 },
-  { tres: 3, cuatro: 4 }
+  { uno: 'tres', dos: 'cuatro' },
+  { uno: 'cinco', dos: 'seis' }
 ]
 */
