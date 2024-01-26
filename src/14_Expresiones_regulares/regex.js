5beacbdd03262920754ab570aa6581915ca595b2
daniel <daniel.pinedamo@amigo.edu.co>
2024-01-25 22:58:16 -0500

    resumen_expresiones_regulares.jpg

--- src/14_Expresiones_regulares/regex.js
+++ src/14_Expresiones_regulares/regex.js
@@ -12,14 +12,28 @@ Documentación Oficial...
 - Expresiones Regulares
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions/Cheatsheet
+
 - RegExp()
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
 
 JavaScript Info - Expresiones Regulares:
 https://javascript.info/regular-expressions
-*/
+
+Wikipedia - Expresiones Regulares:
+https://en.wikipedia.org/wiki/Regular_expression
+
+Secuencia de caracteres que forman un patron de busqueda
+
+Buscan que un string tenga ciertos caracteres
+
+Se usa principalmente en string */
 
 /* --------------------------------------------------- */
 
-/* Ejemplo 1
-https://youtu.be/aPkBloR9MEI?si=NEqljSJQqCioN9_c  */
+/* Ejemplo 1 - Validar Correo */
+
+/* --------------------------------------------------- */
+
+/* Ejemplo 2
+https://youtu.be/aPkBloR9MEI?si=3QOCE_Hw_wWzAoPp */
