0b1412db862bc5f558fab7ee67440f45bbf2d3ce
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-20 23:15:11 -0500

    modulos de JS

--- src/20_Modulos_import_export_require/1_Explicacion_ECMAScript_Modules_import_export/index2.js
+++ src/20_Modulos_import_export_require/1_Explicacion_ECMAScript_Modules_import_export/index2.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-duplicates */
 /* eslint-disable indent */
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
@@ -28,25 +31,48 @@ si se puede importar sin nombre de extension
 ❌ Uncaught ReferenceError: secretNumber is not defined */
 
 import {
-    dividir,
-    multiplicar,
-    restar,
-    secretNumber2,
-    sumar,
+  sumar as add, // export necesita alias (as) para cambiar nombre de funcion importada
+  dividir,
+  Math,
+  multiplicar,
+  nombre,
+  restar,
+  secretNumber2,
+  x, y,
 } from './math.js';
 
-console.log(secretNumber2);
-// 40
+/* export default se importa SIN {},
+NO necesita as para cambiar nombre de importacion */
+import moduloMath from './math.js'; // export default
+console.log(moduloMath);    // 'modulo math'
 
-console.log(
-  sumar(1, 1),
-);
-// 1+1 = 2
+/* export se importa CON {} */
+console.log(nombre);        // 'modulo math'
 
+console.log(secretNumber2); // 40
+
+/* ERROR: no se puede importar y definir el mismo nombre de funcion,
+la solucion a esto es un alias
+const restar = (a, b) => a - b;
+❌ Uncaught SyntaxError: Identifier 'restar' has already been declared */
 console.log(
   restar(10, 5),
 );
 // 10-5 = 5
+
+/* La funcion q defino en index2.js
+const sumar = (a, b) => `'${a} ${b}'`;
+y q exporto en math.js
+export const sumar = (a, b) => a + b;
+tienen el mismo nombre: sumar
+
+add:             importado de math.js, en math.js la funcion se llama sumar
+const sumar ...: funcion q defino en index2.js */
+console.log(add(1, 1));   // 1+1 = 2
+/* console.log(sumar(1, 1));
+❌ Uncaught ReferenceError: sumar is not defined */
+const sumar = (a, b) => `'${a} ${b}'`;
+console.log(sumar(1, 1)); // '1 1'
 
 console.log(
   multiplicar(2, 2),
@@ -57,3 +83,19 @@ console.log(
   dividir(20, 2),
 );
 // 20/2 = 10
+
+console.log(x); // 30
+console.log(y); // 100
+
+// Instanciar clase
+const math = new Math();
+console.log(math);
+// Math {}
+//   [[Prototype]]: Object
+//     constructor: class Math
+//     double: ƒ double(number)
+//     [[Prototype]]: Object
+
+// ejecutar metodo (funcion) double()
+console.log(math.double(2));
+// 2*2 = 4
--- src/20_Modulos_import_export_require/1_Explicacion_ECMAScript_Modules_import_export/math.js
+++ src/20_Modulos_import_export_require/1_Explicacion_ECMAScript_Modules_import_export/math.js
@@ -1,17 +1,38 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable one-var-declaration-per-line */
+/* eslint-disable semi */
+/* eslint-disable one-var */
+/* eslint-disable prefer-const */
+/* eslint-disable import/no-mutable-exports */
 /* eslint-disable import/prefer-default-export */
-/* eslint-disable no-unused-vars */
 
 /* --------------------------------------------------- */
 
 // const secretNumber1 = 30;
 
-export const secretNumber2 = 40;
+export const secretNumber2 = 40;   // const
+export let nombre = 'modulo math'; // let
+// Solamente se puede exportar por defecto UNA SOLA constante, funcion, clase, etc.
+export default nombre;
 
-export const sumar = (a, b) => a + b;
+// exportar varias variables
+export let x = 30, y = 100;
+
+// funcion flecha
 export const restar = (a, b) => a - b;
+export const sumar = (a, b) => a + b;
+
+// funcion regular
 export function multiplicar(a, b) {
   return a * b;
 }
 export function dividir(a, b) {
   return a / b;
+}
+
+// clases
+export class Math {
+  double(number) {
+    return number * number
+  }
 }
