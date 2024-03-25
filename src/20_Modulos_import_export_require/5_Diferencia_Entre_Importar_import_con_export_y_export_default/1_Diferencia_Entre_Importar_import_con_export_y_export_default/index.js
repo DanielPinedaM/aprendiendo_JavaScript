a06d2d2170c10c32afabf53b504eed4b18afb8c9
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-25 18:13:17 -0500

    en carpeta 20_Modulos_import_export_require agregar titulos h1

---
+++ src/20_Modulos_import_export_require/5_Diferencia_Entre_Importar_import_con_export_y_export_default/1_Diferencia_Entre_Importar_import_con_export_y_export_default/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+import sumando, { uno } from './math.js';
+
+console.log(uno);
+// 1
+
+console.log(
+  sumando(2, 2),
+);
+// 2+2 = 4
---
+++ src/20_Modulos_import_export_require/5_Diferencia_Entre_Importar_import_con_export_y_export_default/1_Diferencia_Entre_Importar_import_con_export_y_export_default/math.js
@@ -0,0 +1,8 @@
+export const uno = 1;
+
+const sumar = (a, b) => a + b;
+
+/* Solamente se puede
+exportar por defecto UNA SOLA
+constante, funcion, clase, etc. */
+export default sumar;
