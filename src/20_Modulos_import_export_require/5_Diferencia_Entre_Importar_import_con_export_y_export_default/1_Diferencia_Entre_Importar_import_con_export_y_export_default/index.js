3cd3028c442d366c308094c032f9bf3f13f37cad
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-25 18:28:08 -0500

    1_Diferencia_Entre_Importar_import_con_export_y_export_default

--- src/20_Modulos_import_export_require/5_Diferencia_Entre_Importar_import_con_export_y_export_default/1_Diferencia_Entre_Importar_import_con_export_y_export_default/index.js
+++ src/20_Modulos_import_export_require/5_Diferencia_Entre_Importar_import_con_export_y_export_default/1_Diferencia_Entre_Importar_import_con_export_y_export_default/index.js
@@ -1,4 +1,36 @@
+/* eslint-disable max-len */
 /* eslint-disable import/extensions */
+
+/* --------------------------------------------------------------- */
+
+/*
+Diferencia Entre Importar import con export y export default
+
+                                       |----------------------------------|---------------------------------------------|
+                                       | Exportar por Defecto             | Exportar                                    |
+                                       | export default                   | export                                      |
+|--------------------------------------|----------------------------------|---------------------------------------------|
+| Necesita llaves {} para importar     | ❌                               | ✔️                                          |
+|                                      | import sumando from './math.js'; | import { uno as primero } from './math.js'; |
+|--------------------------------------|----------------------------------|---------------------------------------------|
+| Necesita as para renombrar el modulo | ❌                               | ✔️                                          |
+|                                      |                                  |                                             |
+|                                      | // math.js                       | // math.js                                  |
+|                                      | const sumar = (a, b) => a + b;   | export const uno = 1;                       |
+|                                      | export default sumar;            |                                             |
+|                                      |                                  |                                             |
+|                                      | // index.js                      | // index.js                                 |
+|                                      | import sumando from './math.js'; | import { uno as primero } from './math.js'; |
+|                                      |                                  |                                             |
+|                                      | console.log(                     | console.log(primero);                       |
+|                                      |    sumando(2, 2),                | // 1                                        |
+|                                      | );                               |                                             |
+|                                      | // 2+2 = 4                       |                                             |
+|--------------------------------------|----------------------------------|---------------------------------------------|
+*/
+
+/* --------------------------------------------------------------- */
+
 import sumando, { uno } from './math.js';
 
 console.log(uno);
