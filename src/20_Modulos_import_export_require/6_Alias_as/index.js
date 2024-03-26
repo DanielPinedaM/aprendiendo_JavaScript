ffa1cfb8f56b98b82553a2167718779985a2bce5
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-25 19:36:23 -0500

    6_Alias_as

---
+++ src/20_Modulos_import_export_require/6_Alias_as/index.js
@@ -0,0 +1,27 @@
+/* --------------------------------------------------------------- */
+
+/*
+Las variables numero
+tienen el mismo nombre en index.js y math.js
+
+SINO usara as daria este error
+❌ Uncaught SyntaxError: Indetifier restar 'restar' has already been declared
+
+export necesita alias (as)
+para cambiar nombre de la variable importada
+
+modulo: importado de math.js
+numero: variable q defino en index.js
+*/
+
+import { hola, numero as modulo } from './math';
+
+const numero = 2;
+console.log(numero);
+// 2
+
+console.log(modulo);
+// 1
+
+console.log(hola);
+// 'hola mundo'
---
+++ src/20_Modulos_import_export_require/6_Alias_as/math.js
@@ -0,0 +1,8 @@
+// as en import
+export const numero = 1;
+
+// as en export
+const saludar = 'hola mundo';
+export {
+  saludar as hola,
+};
