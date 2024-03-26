7cddaf10dcbce7cbb80b4bdfb72c5b9d4f45524f
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-25 22:49:13 -0500

    1_Diferencia_ECMAScript_Modules_(import_export)_CommonJS_(require)

--- src/20_Modulos_import_export_require/9_CommonJS_(require)/1_Diferencia_ECMAScript_Modules_(import_export)_CommonJS_(require)/diferencia.js
+++ src/20_Modulos_import_export_require/9_CommonJS_(require)/1_Diferencia_ECMAScript_Modules_(import_export)_CommonJS_(require)/diferencia.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /*
 ¿Diferencias, Similitudes y Cuál Usar Entre import export ECMAScript Modules y require CommonJS?
 
@@ -6,4 +7,42 @@ https://deno.com/blog/commonjs-is-hurting-javascript
 
 BunJS
 https://bun.sh/blog/commonjs-is-not-going-away
+
+Tutorial - Midudev
+https://www.youtube.com/watch?app=desktop&v=yB4n_K7dZV8&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7&index=1&t=1794s
+
+https://www.youtube.com/watch?app=desktop&v=yB4n_K7dZV8&list=PLUofhDIg_38qm2oPOV-IRTTEKyrVBBaU7&index=1&t=2131s
+
+                                      |------------------------------------|------------------------------------|
+                                      | ECMAScript Modules                 | CommonJS                           |
+|-------------------------------------|------------------------------------|------------------------------------|
+| ¿Está obsoleto?                     | ❌                                 | ✔️                                 |
+|                                     | Buena Práctica                     | Mala Práctica                      |
+|-------------------------------------|------------------------------------|------------------------------------|
+| Exportar                            |
+|                                     |
+|                                     |
+|                                     |
+|-------------------------------------|
+| Exportar por defecto                |
+|                                     |
+|                                     |
+|                                     |
+|-------------------------------------|------------------------------------|------------------------------------|
+| ¿Es Necesario Escribir la Extensión | ❌                                 | ❌                                 |
+| en CommonJS y ES Modules?           |------------------------------------|------------------------------------|
+|                                     | En Vanilla JavaScript cuando NO se escribe la extension .js da error    |
+|                                     | ❌ GET http://127.0.0.1:5501/src/ math net::ERR_ABORTED 404 (Not Found) |
+|                                     |                                                                         |
+|                                     | Resolve Extensions:                                                     |
+|                                     | En cambio, en proyectos reales con frameworks y librerías               |
+|                                     |  (NodeJS, NestJS, Angular, React, Vue, etc.)                            |
+|                                     | SI se puede importar sin nombre de extensión                            |
+|-------------------------------------|-------------------------------------------------------------------------|
+|
+|
+|
+
 */
+
+/* --------------------------------------------------- */
