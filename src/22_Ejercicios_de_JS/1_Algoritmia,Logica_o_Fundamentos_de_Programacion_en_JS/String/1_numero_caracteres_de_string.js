621ed26b94712687ce056ccebd03e6b18e5e16b7
daniel <daniel.pinedamo@amigo.edu.co>
2024-03-25 23:00:29 -0500

    1_Algoritmia,Logica_o_Fundamentos_de_Programacion_en_JS

---
+++ src/22_Ejercicios_de_JS/1_Algoritmia,Logica_o_Fundamentos_de_Programacion_en_JS/String/1_numero_caracteres_de_string.js
@@ -0,0 +1,10 @@
+/*
+Tutorial - Jon Mircha
+https://youtu.be/3o5d-tdVscU?si=wv28RhWylyKRZhEw
+
+Programa una función que
+cuente el número de caracteres de una cadena de texto,
+pe. miFuncion("Hola Mundo") devolverá 10.
+*/
+
+/* --------------------------------------------------- */
---
+++ src/22_Ejercicios_de_JS/1_Algoritmia,Logica_o_Fundamentos_de_Programacion_en_JS/String/3_convertir_de_string_a_array.js
@@ -0,0 +1,11 @@
+/*
+Tutorial - Jon Mircha
+https://youtu.be/3o5d-tdVscU?si=wv28RhWylyKRZhEw
+
+Convertir de string '' a array []
+Programa una función que
+dada una String te devuelva un Array de textos separados por cierto caracter,
+pe. miFuncion('hola que tal', ' ') devolverá (3) ['hola', 'que', 'tal']
+*/
+
+/* --------------------------------------------------- */
