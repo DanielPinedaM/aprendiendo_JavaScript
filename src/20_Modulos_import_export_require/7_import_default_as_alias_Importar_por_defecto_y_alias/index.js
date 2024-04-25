a4409c837ca26bcba2081657612e87648d1714f6
daniel <daniel.pinedamo@amigo.edu.co>
2024-04-24 22:42:05 -0500

    7_import_default_as_alias_Importar_por_defecto_y_alias

---
+++ src/20_Modulos_import_export_require/7_import_default_as_alias_Importar_por_defecto_y_alias/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-named-default */
+/* eslint-disable import/named */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable import/extensions */
+
+/* --------------------------------------------------- */
+
+import { default as uno } from './math.js';
+
+console.log(uno); // 1
---
+++ src/20_Modulos_import_export_require/7_import_default_as_alias_Importar_por_defecto_y_alias/math.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable prefer-const */
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable import/prefer-default-export */
+
+/* --------------------------------------------------- */
+
+const uno = 1;
+export default uno;
