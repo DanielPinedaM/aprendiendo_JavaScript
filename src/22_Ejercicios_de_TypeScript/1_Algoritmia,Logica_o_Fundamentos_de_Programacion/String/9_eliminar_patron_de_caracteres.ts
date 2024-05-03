b0931ed1f10d2db60ed674f5e5e4e080b9b269e8
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-02 22:35:31 -0500

    9_eliminar_patron_de_caracteres.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/String/9_eliminar_patron_de_caracteres.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/String/9_eliminar_patron_de_caracteres.ts
@@ -7,6 +7,33 @@ Ejemplo: miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3,
 
 /*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Expresion regular      █
  █ Solucion de Jon Mircha █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
- */
+https://www.youtube.com/watch?v=U4buFGcd_eg&t=1292s
+*/
+
+const eliminarCaracteres = (texto: string = "", patron: string = ""): string | null => {
+    if(!texto) {
+        console.warn("no ingresaste texto");
+        return null;
+    }
+
+    if(!patron) {
+        console.warn("no patron de caracteres");
+        return null;
+    }
+
+    // new RegExp -> Expresion regular
+    // patron     -> lo q se busca en texto
+    // i          -> omitir mayusculas y minusculas
+    // g          -> buscar en todo el texto
+    // ""         -> NO reemplazar caracteres en texto
+    return texto.replace(new RegExp(patron, "ig"), "");
+}
+
+// eliminar "xyz"
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");   // '1, 2, 3, 4 y 5'
+
+// eliminar "letras desde la a hasta la z"
+eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "[a-z]"); // '1, 2, 3, 4  5'
