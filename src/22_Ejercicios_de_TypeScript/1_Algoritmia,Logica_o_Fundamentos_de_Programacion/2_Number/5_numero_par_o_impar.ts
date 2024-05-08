a190deba944117920d0b47314fd188c2d831d939
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-07 23:09:35 -0500

    5_numero_par_o_impar.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/5_numero_par_o_impar.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/5_numero_par_o_impar.ts
@@ -9,6 +9,16 @@ Ejemplo: miFuncion(29) devolverá Impar.
 /*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
  █ Solucion de Jon Mircha                             █
- █ █
+ █ https://www.youtube.com/watch?v=qMjGhYhmrFc&t=730s █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
+// num % 2 === 0 -> num es par
+const numEsPar = (num: number): boolean => num % 2 === 0;
+numEsPar(29); // false
+numEsPar(2);  // true
+
+
+// num % 2 !== 0 -> num es impar
+const numEsImpar = (num: number): boolean => num % 2 !== 0;
+numEsImpar(29); // true
+numEsImpar(2);  // false
--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/6_convertir_grados_celsius_y_fahrenheit.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/6_convertir_grados_celsius_y_fahrenheit.ts
@@ -1,7 +1,11 @@
 /* 
 Programa una función para 
 convertir grados Celsius a Fahrenheit y viceversa.
-Ejemolo: miFuncion(0,"C") devolverá 32°F.
+
+1 grado Celsius    = 33.8 grados Fahrenheit
+1 grado Fahrenheit = -17.2222 grados Celsius
+
+Ejemplo: miFuncion(0,"C") devolverá 32°F.
 */
 
 /* --------------------------------------------------- */
@@ -9,6 +13,6 @@ Ejemolo: miFuncion(0,"C") devolverá 32°F.
 /*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
  █ Solucion de Jon Mircha                             █
- █ █
+ █ https://www.youtube.com/watch?v=qMjGhYhmrFc&t=951s █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
