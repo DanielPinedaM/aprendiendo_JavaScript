c949a5aca94b6c503b39f18379c4afff57d02a55
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-08 23:23:45 -0500

    9_numero_años_desde_una_fecha_hasta_fecha_actual.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/9_numero_años_desde_una_fecha_hasta_fecha_actual.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/9_numero_años_desde_una_fecha_hasta_fecha_actual.ts
@@ -1,4 +1,11 @@
-/* 
+/*
+IMPORTANTE ⚠️
+No hace falta aprender el objeto Date en JS
+ni operaciones con fechas en JS
+porq ya existen librerias para esto
+- DayJS                https://day.js.org/
+- MomentJS (obsoleto): https://momentjs.com/
+
 Programa una función que 
 dada una fecha válida determine 
 cuantos años han pasado hasta el día de hoy.
@@ -8,7 +15,7 @@ Ejemplo: miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
 /* --------------------------------------------------- */
 
 /*
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ Solucion de Jon Mircha                             █
- █  █
- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Solucion de Jon Mircha                           █
+ █ https://youtu.be/WbJzfthCFrM?si=Rowy5es2ojw75ztB █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
