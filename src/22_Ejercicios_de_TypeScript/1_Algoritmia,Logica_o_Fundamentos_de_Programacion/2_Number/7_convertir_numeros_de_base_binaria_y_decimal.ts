f66fab9337dc14bb27a05ac0d662de422af2cdcf
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-08 22:54:35 -0500

    7_convertir_numeros_de_base_binaria_y_decimal.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/7_convertir_numeros_de_base_binaria_y_decimal.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/2_Number/7_convertir_numeros_de_base_binaria_y_decimal.ts
@@ -9,6 +9,23 @@ Ejemplo: miFuncion(100, 2) devolverá 4 base 10.
 /*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
  █ Solucion de Jon Mircha                             █
- █  █
+ █ https://www.youtube.com/watch?v=WbJzfthCFrM&t=91s  █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
+const baseBinariaDecimal = (numero: number, base: 2 | 10): string => {
+  if (typeof numero !== 'number') return '';
+  if (base !== 2 && base !== 10) return '';
+
+  // parseInt(numero.toString(), base) -> convertir de base binaria a base decimal
+  if (base === 2) {
+    return `${numero} base ${base} = ${parseInt(numero.toString(), base)} base 10`;
+
+    // numero.toString(2)                -> convertir de base decimal a base binaria
+  } else {
+    return `${numero} base ${base} = ${numero.toString(2)} base 2`;
+  }
+};
+
+baseBinariaDecimal(100, 2);  // '100 base 2  = 4 base 10'
+baseBinariaDecimal(4, 10);   // '4 base 10   = 100 base 2'
+baseBinariaDecimal(114, 10); // '114 base 10 = 1110010 base 2'
