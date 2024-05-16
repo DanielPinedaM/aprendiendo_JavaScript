3f28ac21c9bda6dd9dfeffe99af8f917581fc815
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-15 21:44:55 -0500

    3_validar_correo.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/3_Expresiones_regulares_(Regex)/3_Validar_correo.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/3_Expresiones_regulares_(Regex)/3_Validar_correo.ts
@@ -1,6 +1,9 @@
 /* 
 Programa una función que 
 valide que un texto sea un email válido.
+
+admitir letras ñ minuscula y Ñ mayuscula
+
 Ejemplo: miFuncion("jonmircha@gmail.com") devuelve true.
 */
 
@@ -10,17 +13,26 @@ Ejemplo: miFuncion("jonmircha@gmail.com") devuelve true.
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
  █ Solucion de Jon Mircha                              █
  █ https://www.youtube.com/watch?v=9BavKzWBr3c&t=1128s █
- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀vv▀ */
 
-const validarCorreo = (texto: string): boolean => {
+ const validarCorreo = (texto: string): boolean => {
   if (typeof texto !== 'string') return false;
 
-  texto = texto.trim();
-
-  if (texto === '') return false;
-  if (!(texto.includes('@'))) return false;
-
-  return
+  return /^[a-zA-Z0-9_.+-ñÑ]+@[a-zA-Z0-9-ñÑ]+\.[a-zA-Z0-9-.]+$/.test(texto.trim())
 };
 
-validarCorreo('jonmircha@gmail.com'); // true
+validarCorreo('jonmircha@gmail.com');        // true 
+validarCorreo('españa@ñgmail.com');          // true
+validarCorreo('usuario@dominio.c');          // true
+
+validarCorreo('usuario_incorrecto@dominio'); // false
+validarCorreo([1, 2, 3]);                    // false
+validarCorreo({ uno: 1 });                   // false
+validarCorreo(0);                            // false
+validarCorreo(null);                         // false
+validarCorreo(undefined);                    // false
+validarCorreo(NaN);                          // false
+validarCorreo(true);                         // false
+validarCorreo(false);                        // false 
+validarCorreo('');                           // false
+validarCorreo(' ');                          // false
