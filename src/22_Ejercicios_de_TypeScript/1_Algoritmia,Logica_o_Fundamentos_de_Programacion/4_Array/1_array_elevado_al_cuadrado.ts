e0117fd6608861516eb80e151945c6289b2c4808
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-15 23:20:27 -0500

    1_array_elevado_al_cuadrado.ts

---
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/1_array_elevado_al_cuadrado.ts
@@ -0,0 +1,17 @@
+/* 
+Programa una función que 
+dado un array numérico 
+devuelve otro array con los números elevados al cuadrado
+
+Ejemplo: mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+*/
+
+/* --------------------------------------------------- */
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Solucion de Jon Mircha                             █
+ █  █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+
+// USAR .map()
