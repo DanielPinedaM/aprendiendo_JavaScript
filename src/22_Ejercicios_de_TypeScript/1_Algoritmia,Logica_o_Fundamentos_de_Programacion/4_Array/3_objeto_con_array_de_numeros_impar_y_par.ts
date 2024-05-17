333eba27179eb6ff4208815b231b7cd591e6ad8c
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-16 23:53:03 -0500

    3_objeto_con_array_de_numeros_impar_y_par.ts

---
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/3_objeto_con_array_de_numeros_impar_y_par.ts
@@ -0,0 +1,21 @@
+/* 
+Programa una función que 
+dado un array de números 
+devuelva un objeto con 2 arreglos 
+en el primero almacena los números pares 
+y en el segundo los impares
+
+Ejemplo;
+miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá { pares: [2,4,6,8,0], impares: [1,3,5,7,9] }
+*/
+
+/* --------------------------------------------------- */
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Solucion de Jon Mircha                              █
+ █ https://www.youtube.com/watch?v=6ZkiOGU4uaA&t=1139s █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+
+
+// USAR .map() y modulo %
