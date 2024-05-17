97fabb94fa192c1940a92701fc0dcf5cd96d32cb
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-16 22:44:29 -0500

    1_array_elevado_al_cuadrado.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/1_array_elevado_al_cuadrado.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/1_array_elevado_al_cuadrado.ts
@@ -9,9 +9,59 @@ Ejemplo: mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 /* --------------------------------------------------- */
 
 /*
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ Solucion de Jon Mircha                             █
- █  █
- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+ ▄▄▄▄▄▄▄▄▄▄
+ █ .map() █
+ ▀▀▀▀▀▀▀▀▀▀ */
+ 
+const arrayElevadoAlCuadrado = (array: number[]): number[] => Array.isArray(array) ? array.map((elemento) => elemento * elemento) : [];
 
-// USAR .map()
+arrayElevadoAlCuadrado([1, 4, 5]); // (3) [ 1, 16, 25 ]
+ 
+arrayElevadoAlCuadrado([]);        // []
+arrayElevadoAlCuadrado({});        // []
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄
+ █ for i++ █
+ ▀▀▀▀▀▀▀▀▀▀▀ */
+
+const arrayElevadoAlCuadrado2 = (array: number[]): number[] => {
+  if (!Array.isArray(array)) return [];
+
+  const arrayElevadoAlCuadrado: number[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    arrayElevadoAlCuadrado.push(array[i] * array[i]);
+  }
+
+  return arrayElevadoAlCuadrado;
+};
+
+arrayElevadoAlCuadrado2([1, 4, 5]); // (3) [ 1, 16, 25 ]
+
+arrayElevadoAlCuadrado2({});        // []
+arrayElevadoAlCuadrado2([]);        // []
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Solucion de Jon Mircha                            █
+ █ for of                                            █
+ █ https://www.youtube.com/watch?v=6ZkiOGU4uaA&t=85s █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+
+const arrayElevadoAlCuadrado3 = (array: number[]): number[] => {
+  if (!Array.isArray(array)) return [];
+
+  const arrayElevadoAlCuadrado: number[] = [];
+
+  for (let elemento of array) {
+    arrayElevadoAlCuadrado.push(elemento * elemento);
+  }
+
+  return arrayElevadoAlCuadrado;
+};
+
+arrayElevadoAlCuadrado3([1, 4, 5]); // (3) [ 1, 16, 25 ]
+
+arrayElevadoAlCuadrado3({});        // []
+arrayElevadoAlCuadrado3([]);        // []
--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/2_numero_mayor_y_menor_de_array.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/2_numero_mayor_y_menor_de_array.ts
@@ -1,5 +1,10 @@
 /* 
+Programa una función que 
+dado un array devuelva el número mas alto 
+y el más bajo de dicho array
 
+Ejemplo:
+miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 */
 
 /* --------------------------------------------------- */
