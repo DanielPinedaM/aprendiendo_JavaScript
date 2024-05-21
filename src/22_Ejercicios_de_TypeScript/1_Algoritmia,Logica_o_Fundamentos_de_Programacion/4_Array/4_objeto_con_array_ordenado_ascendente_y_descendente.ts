5a57ecac7151eea637e2959bb319946f0f58d40a
daniel <daniel.pinedamo@amigo.edu.co>
2024-05-20 23:11:43 -0500

    4_objeto_con_array_ordenado_ascendente_y_descendente.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/4_objeto_con_array_ordenado_ascendente_y_descendente.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/4_objeto_con_array_ordenado_ascendente_y_descendente.ts
@@ -10,11 +10,11 @@ numeros.toSorted((a, b) => a - b);
 letras.toSorted((a, b) => a.localeCompare(b, 'es-ES'));
 
 Ejemplo: 
-miFuncion([2, '4', 3, 1, 'b', 'd', 'c', 'a', true, false, {}, '0']) 
+miFuncion([2, 4, '0', 3, 1, 'b', 'd', 'c', 'a', true, false, [], {}, null, undefined, NaN]) 
 devolverá 
 { 
-  ascendente:  [1, 2, 3, 4, 'a', 'b', 'c', 'd', true, false, {}, '0'],
-  descendente: [4, 3, 2, 1, 'd', 'c', 'b', 'a', true, false, {}, '0']
+  ascendente:  [0, 1, 2, 3, 4, 'a', 'b', 'c', 'd', true, false, [], {}, null, undefined, NaN],
+  descendente: [4, 3, 2, 1, 0, 'd', 'c', 'b', 'a', true, false, [], {}, null, undefined, NaN]
 }
 */
 
@@ -34,10 +34,58 @@ interface IOrdenarArray {
  █  █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
- /*
+/*
  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
  █ .toSorted()   █
  █ .toReversed() █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
+const esNumero = (item: any): boolean => typeof item === 'number' && Number.isNaN(item) === false;
 
+const esStringNumero = (item: any): boolean => typeof item === 'string' && /^\d+$/.test(item.trim());
+
+const esLetra = (item: any): boolean => typeof item === 'string' && /^[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ\s]+$/.test(item.trim());
+
+const ordenarArray = (array: any[]): IOrdenarArray => {
+  const resultado: IOrdenarArray = {
+    ascendente: [],
+    descendente: [],
+  };
+
+  if (!Array.isArray(array)) return resultado;
+  if (!array.length) return resultado;
+
+  // [...array]  spread operator NO modifcar el array original
+  // .filter()   filtrar los numeros [2, 4, '0', 3, 1]
+  // .map()      convertir a tipo numero Number() [2, 4, 0, 3, 1]
+  // .toSorted() ordenar ascendente (de menor a mayor)
+  const numeros: number[] = [...array]
+    .filter((item: any) => esNumero(item) || esStringNumero(item))
+    .map((item: string | number) => Number(item))
+    .toSorted((a: number, b: number) => a - b);
+  console.log(numeros);
+  // (5) [ 0, 1, 2, 3, 4 ]
+
+  const letras: string[] = [...array]
+    .filter((item: any) => esLetra(item))
+    .toSorted((a: string, b: string) => a.localeCompare(b, 'es-ES'));
+  console.log(letras);
+  // (4) [ 'a', 'b', 'c', 'd' ]
+
+  const otrosTiposDeDatos: any[] = array.filter(
+    (item: any) => !(esNumero(item) || esStringNumero(item) || esLetra(item))
+  );
+  console.log(otrosTiposDeDatos);
+  // (7) [ true, false, [], {}, null, undefined, NaN ]
+
+  resultado.ascendente = [...numeros, ...letras, ...otrosTiposDeDatos];
+  resultado.descendente = [...numeros.toReversed(), ...letras.toReversed(), ...otrosTiposDeDatos];
+
+  return resultado;
+};
+
+ordenarArray([2, 4, '0', 3, 1, 'b', 'd', 'c', 'a', true, false, [], {}, null, undefined, NaN]);
+// {
+//   ascendente:  [ 0, 1, 2, 3, 4, 'a', 'b', 'c', 'd', true, false, [], {}, null, undefined, NaN ],
+//   descendente: [ 4, 3, 2, 1, 0, 'd', 'c', 'b', 'a', true, false, [], {}, null, undefined, NaN ]
+// }
