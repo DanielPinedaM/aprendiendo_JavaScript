428c9755720f2159818da5498764003ae7ccc9fe
daniel <daniel.pinedamo@amigo.edu.co>
2024-06-04 14:56:25 -0500

    3_objeto_con_array_de_numeros_impar_y_par.ts

--- src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/3_objeto_con_array_de_numeros_impar_y_par.ts
+++ src/22_Ejercicios_de_TypeScript/1_Algoritmia,Logica_o_Fundamentos_de_Programacion/4_Array/3_objeto_con_array_de_numeros_impar_y_par.ts
@@ -22,23 +22,21 @@ interface IParImpar {
 }
 
 /*
- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ .filter()                                           █
- █ Solucion de Jon Mircha                              █
- █ https://www.youtube.com/watch?v=6ZkiOGU4uaA&t=1139s █
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ Object.groupBy()                                   █
+ █ https://youtu.be/pduSpSe-V-o?si=Qa-YzxFT_PH0X0NY   █
  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
-const parImpar = (array: number[]): IParImpar => {
-  const resultado: IParImpar = {
+const parImpar = (arr: number[]): IParImpar => {
+  let resultado: IParImpar = {
     par: [],
     impar: [],
   };
 
-  if (!Array.isArray(array)) return resultado;
-  if (!array.length) return resultado;
+  if (!Array.isArray(arr)) return resultado;
+  if (!arr.length) return resultado;
 
-  resultado.par = array.filter((elemento: number) => elemento % 2 === 0);
-  resultado.impar = array.filter((elemento: number) => elemento % 2 === 1);
+  resultado = Object.groupBy(arr, (number: number) => (number % 2 === 0 ? 'par' : 'impar'));
 
   return resultado;
 };
@@ -53,26 +51,23 @@ parImpar({}); // { par: [], impar: [] }
 parImpar([]); // { par: [], impar: [] }
 
 /*
- ▄▄▄▄▄▄▄▄▄▄▄
- █ for i++ █
- ▀▀▀▀▀▀▀▀▀▀▀ */
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ .filter()                                           █
+ █ Solucion de Jon Mircha                              █
+ █ https://www.youtube.com/watch?v=6ZkiOGU4uaA&t=1139s █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
 
-const parImpar2 = (array: number[]): IParImpar => {
+const parImpar2 = (arr: number[]): IParImpar => {
   const resultado: IParImpar = {
     par: [],
     impar: [],
   };
 
-  if (!Array.isArray(array)) return resultado;
-  if (!array.length) return resultado;
+  if (!Array.isArray(arr)) return resultado;
+  if (!arr.length) return resultado;
 
-  for (let i = 0; i < array.length; i++) {
-    const elemento: number = array[i];
-
-    elemento % 2 === 0 
-        ? resultado.par.push(elemento) 
-          : resultado.impar.push(elemento);
-  }
+  resultado.par = arr.filter((elemento: number) => elemento % 2 === 0);
+  resultado.impar = arr.filter((elemento: number) => elemento % 2 === 1);
 
   return resultado;
 };
@@ -87,20 +82,26 @@ parImpar2({}); // { par: [], impar: [] }
 parImpar2([]); // { par: [], impar: [] }
 
 /*
- ▄▄▄▄▄▄▄▄▄▄▄▄▄
- █ .reduce() █
- ▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+ ▄▄▄▄▄▄▄▄▄▄▄
+ █ for i++ █
+ ▀▀▀▀▀▀▀▀▀▀▀ */
 
-const parImpar3 = (array: number[]): IParImpar => {
-  if (!Array.isArray(array)) return { par: [], impar: [] };
-  if (!array.length) return { par: [], impar: [] };
+const parImpar3 = (arr: number[]): IParImpar => {
+  const resultado: IParImpar = {
+    par: [],
+    impar: [],
+  };
 
-  return array.reduce<IParImpar>((acc: IParImpar, elemento: number) => {
-      (elemento % 2 === 0 ? acc.par : acc.impar).push(elemento);
-      return acc;
-    },
-    { par: [], impar: [] }
-  );
+  if (!Array.isArray(arr)) return resultado;
+  if (!arr.length) return resultado;
+
+  for (let i = 0; i < arr.length; i++) {
+    const elemento: number = arr[i];
+
+    elemento % 2 === 0 ? resultado.par.push(elemento) : resultado.impar.push(elemento);
+  }
+
+  return resultado;
 };
 
 parImpar3([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
@@ -111,3 +112,64 @@ parImpar3([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 
 parImpar3({}); // { par: [], impar: [] }
 parImpar3([]); // { par: [], impar: [] }
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ .reduce() █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+
+const parImpar4 = (arr: number[]): IParImpar => {
+  if (!Array.isArray(arr)) return { par: [], impar: [] };
+  if (!arr.length) return { par: [], impar: [] };
+
+  return arr.reduce<IParImpar>(
+    (acc: IParImpar, elemento: number) => {
+      (elemento % 2 === 0 ? acc.par : acc.impar).push(elemento);
+      return acc;
+    },
+    { par: [], impar: [] }
+  );
+};
+
+parImpar4([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+// {
+//   par:   [ 2, 4, 6, 8, 0 ],
+//   impar: [ 1, 3, 5, 7, 9 ]
+// }
+
+parImpar4({}); // { par: [], impar: [] }
+parImpar4([]); // { par: [], impar: [] }
+
+/*
+ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+ █ .forEach() █
+ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ */
+
+const parImpar5 = (arr: number[]): IParImpar => {
+  const resultado: IParImpar = {
+    par: [],
+    impar: [],
+  };
+
+  if (!Array.isArray(arr)) return resultado;
+  if (!arr.length) return resultado;
+
+  arr.forEach((number: number) => {
+    let key: string = 'par';
+    if (number % 2 !== 0) {
+      key = 'impar';
+    }
+    resultado[key].push(number);
+  });
+
+  return resultado;
+};
+
+parImpar5([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+// {
+//   par:   [ 2, 4, 6, 8, 0 ],
+//   impar: [ 1, 3, 5, 7, 9 ]
+// }
+
+parImpar5({}); // { par: [], impar: [] }
+parImpar5([]); // { par: [], impar: [] }
